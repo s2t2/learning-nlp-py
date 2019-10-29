3a73ef3f551e32a0664311d91a51d2d71f07a816
Mike Rossetti <s2t2@users.noreply.github.com>
2019-10-29 15:07:01 -0400

    Test lancaster stemmer

--- test/stemma_lemma_test.py
+++ test/stemma_lemma_test.py
@@ -1,8 +1,5 @@
-#
-# STEMMING / LEMMATIZATION
-#
 
-from nltk.stem import PorterStemmer
+from nltk.stem import PorterStemmer, LancasterStemmer
 from nltk.stem.wordnet import WordNetLemmatizer
 
 WORDS = ["is", "are", "be", "was", "ran", "running", "from", "wolves"]
@@ -10,24 +7,16 @@ DOCUMENT = " ".join(WORDS) #> 'is are be was ran running from wolves'
 
 def test_nltk_porter_stemmer():
     ps = PorterStemmer()
+    assert [ps.stem(word) for word in WORDS] == ["is", "are", "be", "wa", "ran", "run", "from", "wolv"]
 
-    assert [ps.stem(word) for word in WORDS] == [
-        "is", "are", "be", "wa",
-        "ran", "run", "from", "wolv"
-    ]
+def test_nltk_lancaster_stemmer():
+    ls = LancasterStemmer()
+    assert [ls.stem(word) for word in WORDS] == ["is", "ar", "be", "was", "ran", "run", "from", "wolv"]
 
 def test_nltk_lemmatizer():
     lm = WordNetLemmatizer()
-
-    assert [lm.lemmatize(word) for word in WORDS] == [
-        "is", "are", "be", "wa",
-        "ran", "running", "from", "wolf"
-    ]
+    assert [lm.lemmatize(word) for word in WORDS] == ["is", "are", "be", "wa", "ran", "running", "from", "wolf"]
 
 def test_spacy_lemmatizer(nlp):
     doc = nlp(DOCUMENT) #> <class 'spacy.tokens.doc.Doc'>
-
-    assert [word.lemma_ for word in doc] == [
-        "be", "be", "be", "be",
-        "run", "run", "from", "wolf"
-    ]
+    assert [word.lemma_ for word in doc] == ["be", "be", "be", "be", "run", "run", "from", "wolf"]
