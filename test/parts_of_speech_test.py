54c3a6873d10fd0a7302102ae6bceea62ba379c2
Mike Rossetti <s2t2@users.noreply.github.com>
2019-10-28 21:57:44 -0400

    Test spacy parts of speech detection

---
+++ test/parts_of_speech_test.py
@@ -0,0 +1,17 @@
+
+from conftest import MY_PREAMBLE
+
+def test_spacy_parts_of_speech(nlp):
+    doc = nlp("Hello World!!!") #> <class 'spacy.tokens.doc.Doc'>
+    assert [str(token) for token in doc] == ["Hello", "World", "!", "!", "!"]
+    assert [token.pos_ for token in doc] == []
+
+    doc = nlp(MY_PREAMBLE) #> <class 'spacy.tokens.doc.Doc'>
+    assert [str(token) for token in doc] == [
+        'Friends', ',', 'Romans', ',', 'countrymen', ',',
+        'lend', 'me', 'your', 'ears', ';', '911'
+    ]
+    assert [token.pos_ for token in doc] == [
+        'NOUN', 'PUNCT', 'PROPN', 'PUNCT', 'NOUN', 'PUNCT',
+        'VERB', 'PRON', 'PRON', 'NOUN', 'PUNCT', 'NUM'
+    ]
