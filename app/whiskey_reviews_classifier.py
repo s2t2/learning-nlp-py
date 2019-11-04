d383a519e3327a5dbb26f86edb7cba73d605d750
Mike Rossetti <s2t2@users.noreply.github.com>
2019-11-04 17:07:50 -0500

    Loop through training dataset

--- app/whiskey_reviews_classifier.py
+++ app/whiskey_reviews_classifier.py
@@ -1,22 +1,40 @@
 
+# FYI CATEGORIES = {1: "Scotch", 2: "Bourbon/Tennessee", 3: "Craft Whiskey", 4: "Canadian"}
+
 import os
 
 import pandas as pd
 
-DATA_DIRPATH = os.path.join(os.path.dirname(__file__), "..", "data", "whiskey")
+WHISKEY_DATA_DIRPATH = os.path.join(os.path.dirname(__file__), "..", "data", "whiskey")
+SUBMISSION_CSV_FILEPATH = os.path.join(WHISKEY_DATA_DIRPATH, "my_submission.csv")
 
-if __name__ == "__main__":
+def generate_submissions(model, csv_filepath=SUBMISSION_CSV_FILEPATH):
+    testing_df = pd.read_csv(os.path.join(WHISKEY_DATA_DIRPATH, "test.csv"))
 
-    training_df = pd.read_csv(os.path.join(DATA_DIRPATH, "train.csv"))
-    testing_df = pd.read_csv(os.path.join(DATA_DIRPATH, "test.csv"))
+    submission_df = testing_df
 
-    print(training_df.head())
+    # use the model to predict categories for all texts in the testing set
 
-    #breakpoint()
+    breakpoint()
 
+    # then write those to SUBMISSION_CSV_FILEPATH
     # TODO: create an example submission CSV file resembling:
     # id,category
     # 955,1
     # 3532,3
     # 1390,2
     # 1024,4
+    # and return the dataframe for good measure
+
+    return submission_df
+
+if __name__ == "__main__":
+
+    training_df = pd.read_csv(os.path.join(WHISKEY_DATA_DIRPATH, "train.csv"))
+    print(training_df.head())
+    print(training_df["category"].value_counts())
+
+    for i, row in training_df.iterrows():
+        print(row["id"], row["category"], row["description"][0:90])
+
+    #breakpoint()
