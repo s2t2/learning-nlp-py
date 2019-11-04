bb8d162c582c1f33586b64f124aabf231144256e
Mike Rossetti <s2t2@users.noreply.github.com>
2019-11-04 17:17:02 -0500

    Choose an example model to classify whiskey reviews

--- app/whiskey_reviews_classifier.py
+++ app/whiskey_reviews_classifier.py
@@ -4,6 +4,12 @@
 import os
 
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+#from sklearn.linear_model import LogisticRegression
+#from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
+
 
 WHISKEY_DATA_DIRPATH = os.path.join(os.path.dirname(__file__), "..", "data", "whiskey")
 SUBMISSION_CSV_FILEPATH = os.path.join(WHISKEY_DATA_DIRPATH, "my_submission.csv")
@@ -32,9 +38,27 @@ if __name__ == "__main__":
 
     training_df = pd.read_csv(os.path.join(WHISKEY_DATA_DIRPATH, "train.csv"))
     print(training_df.head())
-    print(training_df["category"].value_counts())
+    #print(training_df["category"].value_counts())
+    #> 1    1637
+    #> 2     449
+    #> 3     300
+    #> 4     200
 
-    for i, row in training_df.iterrows():
-        print(row["id"], row["category"], row["description"][0:90])
+    #for i, row in list(training_df.iterrows())[0:4]:
+    #    print(row["id"], row["category"], row["description"][0:90])
+
+    xtrain = training_df["description"]
+    ytrain = training_df["category"]
+
+    tv = TfidfVectorizer()
+    tv.fit(xtrain)
+    matrix = tv.transform(xtrain)
+
+    rf = RandomForestClassifier()
+    rf.fit(matrix.todense(), ytrain)
+    predictions = rf.predict(matrix)
+
+    accy = accuracy_score(ytrain, predictions)
+    print("TRAINING ACCY:", accy)
 
     #breakpoint()
