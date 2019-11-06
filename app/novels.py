050ab58b7ab547a847454b90b3b8fc78b8127ef0
Mike Rossetti <s2t2@users.noreply.github.com>
2019-11-05 20:15:34 -0500

    Test streaming construction of dictionaries

--- app/novels.py
+++ app/novels.py
@@ -3,12 +3,14 @@ import os
 
 import numpy as np
 import pandas as pd
-#import re
+import re
 
 from gensim.corpora import Dictionary
 #from gensim.models.ldamulticore import LdaMulticore
 #from gensim.utils import simple_preprocess
 #from gensim.parsing.preprocessing import STOPWORDS
+
+ALPHANUMERIC_PATTERN = r'[^a-zA-Z ^0-9]'
 
 NOVELS_DIRPATH = os.path.join(os.path.dirname(__file__), "..", "data", "novels")
 
@@ -17,7 +19,9 @@ NOVELS_DIRPATH = os.path.join(os.path.dirname(__file__), "..", "data", "novels")
 # See Corpus Streaming – One Document at a Time for details.
 
 def tokenize(txt):
-    tokens = txt.strip("\n").lower().split()
+    #tokens = txt.strip("\n").lower().split()
+    txt = re.sub(ALPHANUMERIC_PATTERN, "", txt)
+    tokens = txt.lower().split()
     #bow = dictionary.doc2bow(tokens)
     return tokens
 
@@ -30,37 +34,26 @@ def token_stream(path):
             yield tokens
 
 if __name__ == "__main__":
-    #
-    # corpora.Dictionary
-    #   + encapsulates the mapping between normalized words and their integer ids
-    #   + defines the vocabulary of all words that our processing knows about
 
-    texts = ["all the kings men", "ate all the kings hens", "until they all got tired and went to sleep zzz"]
-    token_sets = [tokenize(text) for text in texts]
-    d = Dictionary(token_sets)
-    #print(list(d.items()))
-    print(d.token2id) #> {'all': 0, 'kings': 1, 'men': 2, 'the': 3, 'ate': 4, 'hens': 5, 'and': 6, 'got': 7, 'sleep': 8, 'they': 9, 'tired': 10, 'to': 11, 'until': 12, 'went': 13, 'zzz': 14}
+
+
 
     new_tokens = ["all", "kings", "queens", "jacks"]
     print(new_tokens)
     print(d.doc2idx(new_tokens))
     print(d.doc2bow(new_tokens))
-    #bow_corpus = [d.doc2bow(text) for text in texts]
 
-    #breakpoint()
+    bow_corpus = [d.doc2bow(text) for text in texts]
     #lda = LdaMulticore(corpus=token_sets, id2word=dictionary)
-    #print(lda.print_topics())
-
-
-
-
-
+    lda = LdaMulticore(corpus=bow_corpus, id2word=dictionary)
+    print(lda.print_topics())
 
 
 
     dictionary = Dictionary(token_stream(NOVELS_DIRPATH))
     print(dictionary.token2id)
+    token_counts = [dictionary.doc2bow(tokens) for tokens in token_stream(NOVELS_DIRPATH)]
 
     #dictionary.filter_extremes(no_below=10, no_above=0.75)
-    #lda = LdaMulticore(corpus=token_sets, id2word=dictionary, random_state=723812, num_topics=15, passes=10, workers=4)
+    lda = LdaMulticore(corpus=token_counts, id2word=dictionary, random_state=723812, num_topics=15, passes=10, workers=4)
     #print(lda.print_topics())
--- test/dictionaries_test.py
+++ test/dictionaries_test.py
@@ -1,11 +1,18 @@
+
+import os
+
+from gensim.corpora import Dictionary
+
+from app.novels import token_stream
+from conftest import TOKEN_SETS
+
+NOVELS_DIRPATH = os.path.join(os.path.dirname(__file__), "data", "novels")
+
 #
 # gensim.corpora.Dictionary
 #   + encapsulates the mapping between normalized words and their integer ids
 #   + defines the vocabulary of all words that our processing knows about
 #
-from gensim.corpora import Dictionary
-
-from conftest import TOKEN_SETS
 
 def test_gensim_dictionaries():
 
@@ -22,4 +29,25 @@ def test_gensim_dictionaries():
         [(0, 1), (1, 1), (2, 1), (3, 1)],
         [(0, 1), (1, 1), (3, 1), (4, 1), (5, 1)],
         [(0, 1), (6, 1), (7, 1), (8, 1), (9, 1), (10, 1), (11, 1), (12, 1), (13, 1), (14, 1)]
+    ]
+
+def test_streaming_dictionaries():
+
+    generator = token_stream(NOVELS_DIRPATH)
+
+    dictionary = Dictionary(generator)
+
+    token_items = list(dictionary.items())
+    assert len(token_items) == 1969
+    assert token_items[0:4] == [(0, 'a'), (1, 'about'), (2, 'accommodate'), (3, 'admire')]
+
+    tokens_map = dictionary.token2id
+    assert len(tokens_map.keys()) == 1969
+    assert list(tokens_map.items())[0:4] == [('a', 0), ('about', 1), ('accommodate', 2), ('admire', 3)]
+
+    token_counts = [dictionary.doc2bow(token_set) for token_set in TOKEN_SETS]
+    assert token_counts == [
+        [(8, 1), (378, 1)],
+        [(8, 1), (378, 1)],
+        [(8, 1), (13, 1), (167, 1), (383, 1), (389, 1), (1199, 1), (1714, 1)]
     ]
